0fe78144c2d5dffce5692e43c4a0be5930a983d6
giserliyang <liyang_qifengle@126.com>
2023-05-04 23:41:48 +0800

    feat:构建——打包方案(rollup)2

--- cp2/clone/config/rollup.config.aio.js
+++ cp2/clone/config/rollup.config.aio.js
@@ -1,17 +1,28 @@
 // rollup.config.js
-// commonjs
+// umd
+var nodeResolve = require('rollup-plugin-node-resolve')
+var commonjs = require('rollup-plugin-commonjs')
+
 var common = require('./rollup.js')
 
 module.exports = {
   input: 'src/index.js',
   output: {
-    file: 'dist/index.js',
-    format: 'cjs',
+    file: 'dist/index.aio.js',
+    format: 'umd',
     // When export and export default are not used at the same time, set legacy to true.
     // legacy: true,
+    name: common.name,
     banner: common.banner,
   },
   plugins: [
-    // common.getCompiler()
+    nodeResolve({
+      main: true,
+      extensions: ['.js']
+    }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    common.getCompiler(),
   ]
 }
